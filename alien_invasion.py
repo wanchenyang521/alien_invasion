64cb7f8a4812827fa16998ee6206af0680562772
WanChenyang521 <wan1183254286@icloud.com>
2019-01-24 20:15:47 +0800

    显示飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,27 @@
 import sys
 import pygame
 from setting import Setting
-
+from ship import Ship
 def run_game():
     # 进行初始化屏幕
     pygame.init()
     # 创建设置对象
     ai_setting = Setting()
     # 设置了屏幕大小和游戏标题
-    screen = pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # 创建飞船对象
+    ship = Ship(screen)
+
     # 开始游戏的主循环
     while True:
-
         # 监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 #         每次循环重绘屏幕
         screen.fill(ai_setting.bg_color)
+        ship.blitme()
 #         让绘制的屏幕可见
         pygame.display.flip()
 
---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship():
+
+    # 初始化飞船
+    def __init__(self,screen):
+        self.screen = screen
+
+        # 加载图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
