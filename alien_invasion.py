f440c017b7b08af8fe80de0e3d804935f7e412e6
WanChenyang521 <wan1183254286@icloud.com>
2019-01-24 19:50:24 +0800

    添加游戏设置文件

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,15 @@
 import sys
 import pygame
-
+from setting import Setting
 
 def run_game():
     # 进行初始化屏幕
     pygame.init()
+    # 创建设置对象
+    ai_setting = Setting()
     # 设置了屏幕大小和游戏标题
-    screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode((ai_setting.screen_width,ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # 设置背景颜色
-    bg_color = (230,230,230)
     # 开始游戏的主循环
     while True:
 
@@ -18,7 +18,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 #         每次循环重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_setting.bg_color)
 #         让绘制的屏幕可见
         pygame.display.flip()
 
