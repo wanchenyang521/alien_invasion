335a5a734b67207f73c54d9501b3662da3270560
WanChenyang521 <wan1183254286@icloud.com>
2019-01-24 20:52:52 +0800

    重构check_events()

--- ship.py
+++ ship.py
@@ -25,8 +25,8 @@ class Ship():
 
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.settings.ship_speed_factor
         self.rect.centerx = self.center
